d09922073e188ec540e959a10400efe19b3aa2a9
hexiou <140586099+hexiou@users.noreply.github.com>
2023-07-26 17:05:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask,render_template,request
 import os
 import replicate
+import json
+import time
+import requests
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_Ck2kkbp4xm8WENxpzlUkWQpabICdQL01iDLjW"
-m = replicate.models.get("tstramer/midjourney-diffusion")
-version = m.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
+os.environ["REPLICATE_API_TOKEN"] = "r8_YkBJArF7uNaYCsSAV5XUkftFf86JqV13YSE2X"
 app=Flask(__name__)
 
 
@@ -18,18 +13,15 @@ app=Flask(__name__)
 def index():
     if request.method =="POST":
         q = request.form.get("question")
-        i = {"prompt":q}
-        r=version.predict(**i)
-        return(render_template("index.html",result=r[0]))
+        body = json.dumps({"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", "input": { "prompt": q } })
+        headers = {'Authorization': 'Token r8_YkBJArF7uNaYCsSAV5XUkftFf86JqV13YSE2X','Content-Type': 'application/json'}
+        output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=headers)
+        time.sleep(10)
+        get_url = output.json()['urls']['get']
+        r = requests.post(get_url,headers=headers).json()['output']
+        return(render_template("index.html", result=r[0]))
     else:
-        return(render_template("index.html",result="waiting……"))
+        return(render_template("index.html", result="waiting"))
 
 if __name__=="__main__":
     app.run()
-
-
-# In[ ]:
-
-
-
-
